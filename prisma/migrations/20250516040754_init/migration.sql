ab095108630451fcb49479bd8883ea20a94e41e7
MichaelMira <michaelmirisciotta@gmail.com>
2025-05-15 23:39:00 -0700

    Fix: Remove problematic Post table drop from init migration

--- prisma/migrations/20250516040754_init/migration.sql
+++ prisma/migrations/20250516040754_init/migration.sql
@@ -5,16 +5,10 @@
   - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropForeignKey
-ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
-
 -- AlterTable
 ALTER TABLE "users" DROP COLUMN "password",
 ADD COLUMN     "emailVerified" TIMESTAMP(3),
 ADD COLUMN     "image" TEXT;
-
--- DropTable
-DROP TABLE "Post";
 
 -- CreateTable
 CREATE TABLE "Account" (
